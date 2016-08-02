5998b814d66eed511ad3f76f4e37f38fd5472aae
Adam Jones <adamjns802@gmail.com>
2016-08-02 16:24:53 -0400

    progress through 8/2

--- js/comics.js
+++ js/comics.js
@@ -1,7 +1,5 @@
-/*picture, title/name, description & price of each Comic*/
 $( document ).ready(function() {
     console.log( "ready!" );
-
 
 $.ajax({
 	url: 'http://gateway.marvel.com:80/v1/public/comics?apikey=a3d450fd87cce2aeff11efbcc111f789',
@@ -15,9 +13,11 @@ $.ajax({
 		var allMarvelComics = marvelAPI.data.results;
 
 
-		allMarvelComics.forEach(function (comic) {
-			var comicThumbnail = comic.thubmnail.path+"."+comic.thumbnail.extension;
-			$(".NAMETHISSOMETHING").append("<img src='"+comicThumbnail+"'>");
+
+		comics.forEach(function (comic){
+    	var comicImage = comic.thumbnail.path+"."+comic.thumbnail.extension;
+    	$(".Memphis").append('<div class="col-lg-4 comic leftTop"><div class="column secondLeft"><img class="comicImage" src='+comicImage+'></div><h3>'+comic.title+'</h3><div class="moreInfo"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">More Info</button></div><div class="collapse" id="collapseExample"><div class="card card-block"></div></div>');
+
 
  });
 
