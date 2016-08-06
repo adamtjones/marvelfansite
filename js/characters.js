b3dfe231bf8b3ccbe695aef84a9c9401f7e11397
Adam Jones <adamjns802@gmail.com>
2016-08-06 11:48:07 -0400

    API connection working

--- js/characters.js
+++ js/characters.js
@@ -1,6 +1,4 @@
-/*name & picture of all characters*/
-/*underneath each, "Learn More" showing: Description & List of all Comics
-character appears in*/
+
 $( document ).ready(function() {
     console.log( "ready!" );
 
@@ -8,7 +6,7 @@ $( document ).ready(function() {
 $.ajax({
 	url: 'http://gateway.marvel.com:80/v1/public/characters?apikey=a3d450fd87cce2aeff11efbcc111f789',
 	success: function successHandler(marvelAPI) {
-		console.log(data);
+		console.log(marvelAPI);
 
 	function getRandomArbitrary(e) {
 			return Math.floor(Math.random() * e) + 1;
@@ -17,9 +15,19 @@ $.ajax({
 		var allMarvelCharacters = marvelAPI.data.results;
 
 
-		 characters.forEach(function (character){
+		// function getComics(comic) {
+		// 	var bigString = "";
+		// 	allMarvelComics.forEach(function(comic) {
+		// 		bigString += `<p>${comic.name}</p>`;
+		// 	});
+
+		// 	return bigString;
+		// }
+
+		 allMarvelCharacters.forEach(function (character){
     	var characterPicture = character.thumbnail.path+"."+character.thumbnail.extension;
-	    var characterComic = character.comics.items+character.comics.name;
+	    var characterComic = character.comics.items;
+	    //characterComic = getComics(characterComic);
     	$(".Dallas").append('<div class="col-lg-4 characters secondLeft"><div class="box topLeft"><img class="characterImage" src='+characterPicture+'></div><h3>'+character.name+'</h3><div class="moreInfo"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">More Info</button></div><div class="collapse" id="collapseExample"><div class="card card-block"><p class="charDescription">'+character.description+'</p><p class="charComic">'+characterComic+'</p></div></div>');
 
  });
--- js/comics.js
+++ js/comics.js
@@ -1,10 +1,12 @@
+
 $( document ).ready(function() {
     console.log( "ready!" );
+
 
 $.ajax({
 	url: 'http://gateway.marvel.com:80/v1/public/comics?apikey=a3d450fd87cce2aeff11efbcc111f789',
 	success: function successHandler(marvelAPI) {
-		console.log(data);
+		console.log(marvelAPI);
 
 	function getRandomArbitrary(e) {
 			return Math.floor(Math.random() * e) + 1;
@@ -13,11 +15,21 @@ $.ajax({
 		var allMarvelComics = marvelAPI.data.results;
 
 
+		// function buildPrices(prices) {
+		// 	var bigString = "";
+		// 	prices.forEach(function(price) {
+		// 		bigString += `<p>${price.price}</p>`;
+		// 	});
 
-		comics.forEach(function (comic){
-    	var comicImage = comic.thumbnail.path+"."+comic.thumbnail.extension;
-    	$(".Memphis").append('<div class="col-lg-4 comic leftTop"><div class="column secondLeft"><img class="comicImage" src='+comicImage+'></div><h3>'+comic.title+'</h3><div class="moreInfo"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">More Info</button></div><div class="collapse" id="collapseExample"><div class="card card-block"></div></div>');
+		// 	return bigString;
+		// }
 
+		 allMarvelComics.forEach(function (comic){
+    	var comicPicture = comic.thumbnail.path+"."+comic.thumbnail.extension;
+	    var comicPrice = comic.prices;
+	    //comicPrice = buildPrices(comicPrice);
+	    //characterComic = getComics(characterComic);
+    	$(".Memphis").append('<div class="col-lg-4 characters secondLeft"><div class="box topLeft"><img class="characterImage" src='+comicPicture+'></div><h3>'+comic.title+'</h3><div class="moreInfo"><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">More Info</button></div><div class="collapse" id="collapseExample"><div class="card card-block"><p class="charDescription">'+comic.price+'</p><p class="charComic"></p></div></div>');
 
  });
 
